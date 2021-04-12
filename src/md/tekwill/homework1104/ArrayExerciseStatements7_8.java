7a9592c3869e4f19d0820d5a21bf476382b6d2d8
Roxana111 <roxanamehryar@gmail.com>
2021-04-12 11:56:37 +0300

    add homework1104 homework updated examples

--- src/md/tekwill/homework1104/ArrayExerciseStatements7_8.java
+++ src/md/tekwill/homework1104/ArrayExerciseStatements7_8.java
@@ -12,27 +12,28 @@ public class ArrayExerciseStatements7_8 {
         int sum = 0;
         for (int j : c) {
             sum += j;
-            //d) Copy 11-element array a into the first portion of array b, which contains 34 elements"
-            int a[] = new int[11];
-            int b[] = new int[34];
-            for (int i = 0; i < a.length; i++) {
-                b[i] = a[i];
+        }
+        //d) Copy 11-element array a into the first portion of array b, which contains 34 elements"
+        int a[] = new int[11];
+        int b[] = new int[34];
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i];
+        }
+        //e) Determine and display the smallest and largest values contained in 99-element floating-point array w.
+        float w[] = new float[99];
+        float min = w[0];
+        float max = w[0];
+        for (float v : w) {
+            if (min > v) {
+                min = v;
+            } else if (max < v) {
+                max = v;
             }
-            //e) Determine and display the smallest and largest values contained in 99-element floating-point array w.
-            float w[] = new float[99];
-            float min = w[0];
-            float max = w[0];
-            for (float v : w) {
-                if (min > v) {
-                    min = v;
-                } else if (max < v) {
-                    max = v;
-                }
-            }
-
-            System.out.println("largest value is: " + max);
-            System.out.println("smallest value is: " + min);
         }
 
+        System.out.println("largest value is: " + max);
+        System.out.println("smallest value is: " + min);
     }
+
 }
+
