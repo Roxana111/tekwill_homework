ec665ab40d2423b051ede3225708c91c72087e52
Roxana Mehryar-Rad <roxanamehryar-rad@Roxanas-MacBook-Pro.local>
2021-04-27 16:32:33 +0300

    add homework2704 updated examples

--- src/md/tekwill/homework2704/PerimeterCalculator.java
+++ src/md/tekwill/homework2704/PerimeterCalculator.java
@@ -5,63 +5,63 @@ import java.util.Scanner;
 public class PerimeterCalculator {
 
     public static void main(String[] args) {
-        new PerimeterCalculator().process();
-    }
-
-    public static double findperimeter(int number, double pi, double radius) {
-        return (number * pi * radius);    //perimeter of circle
-    }
-
-    public static double findperimeter(int number2, double length) {
-        return (number2 * length);       //perimeter of square
-    }
-
-    public static double findperimeter(int number3, int lengthrec, int widthrec) {
-        return (number3 * (lengthrec + widthrec));//perimter of rectangle
-    }
-
-    public static double findperimeter(double base, double height, double hypothenuse) {
-        return (base + height + hypothenuse); //perimeter of triangle
-    }
-
-    public double findperimeter(double alength, double blength, double clength, double dlength) {
-
-        return (alength + blength + clength + dlength);//perimeter of quadrilateral
-    }
-
-    private void process() {
-        final int number = 2;
-        final int number2 = 4;
-        final int number3 = 2;
-        final double pi = Math.PI;
-
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter length of first triangle edge");
+        int triangleEdge1 = in.nextInt();
+        System.out.println("Enter length of second triangle edge");
+        int triangleEdge2 = in.nextInt();
+        System.out.println("Enter length of third triangle edge");
+        int triangleEdge3 = in.nextInt();
+        System.out.println("Perimeter of triangle is "+PerimeterCalculator.calculate(triangleEdge1, triangleEdge2, triangleEdge3));
+
         System.out.println("Enter square length");
-        double length = in.nextDouble();
+        int squarelength = in.nextInt();
+        System.out.println("Perimeter of square is "+PerimeterCalculator.calculate(squarelength));
+
+        System.out.println("Enter quadrilateral first length");
+        int quadrilength1 = in.nextInt();
+        System.out.println("Enter quadrilateral second length");
+        int quadrilength2 = in.nextInt();
+        System.out.println("Enter quadrilateral third length");
+        int quadrilength3 = in.nextInt();
+        System.out.println("Enter quadrilateral fourth length");
+        int quadrilength4 = in.nextInt();
+        System.out.println("Perimeter of quadrilateral is "+PerimeterCalculator.calculate(quadrilength1, quadrilength2, quadrilength3, quadrilength4));
+
         System.out.println("Enter rectangle length");
         int lengthrec = in.nextInt();
         System.out.println("Enter rectangle width");
         int widthrec = in.nextInt();
+        System.out.println("Perimeter of rectangle is "+PerimeterCalculator.calculate(lengthrec, widthrec));
+
         System.out.println("Enter circle radius");
-        int radius = in.nextInt();
-        System.out.println("Enter triangle base");
-        double base = in.nextDouble();
-        System.out.println("Enter triangle hypothenuse");
-        double hypothenuse = in.nextDouble();
-        System.out.println("Enter triangle height");
-        double height = in.nextDouble();
-        System.out.println("Enter quadrilateral first length");
-        double alength = in.nextDouble();
-        System.out.println("Enter quadrilateral second length");
-        double blength = in.nextDouble();
-        System.out.println("Enter quadrilateral third length");
-        double clength = in.nextDouble();
-        System.out.println("Enter quadrilateral fourth length");
-        double dlength = in.nextDouble();
-        System.out.println("perimeter of circle is " + findperimeter(number, pi, radius));
-        System.out.println("perimeter of square is " + findperimeter(number2, length));
-        System.out.println("perimeter of quadrilateral is " + findperimeter(alength, blength, clength, dlength));
-        System.out.println("perimeter of triangle is " + findperimeter(base, height, hypothenuse));
-        System.out.println("perimeter of rectangle is " + findperimeter(number3, lengthrec, widthrec));
+        double radius = in.nextDouble();
+        System.out.println("Perimeter of circle is "+PerimeterCalculator.calculate(radius));
+
+
     }
+
+    public static double calculate(double radius) {
+        return (2 * Math.PI * radius);    //perimeter of circle
+    }
+
+    public static double calculate(int squarelength) {
+        return (4 * squarelength);       //perimeter of square
+    }
+
+    public static double calculate(int lengthrec, int widthrec) {
+        return (2 * (lengthrec + widthrec));//perimeter of rectangle
+    }
+
+    public static double calculate(int triangleEdge1, int triangleEdge2, int triangleEdge3) {
+        return (triangleEdge1 + triangleEdge2 + triangleEdge3); //perimeter of triangle
+    }
+
+    public static double calculate(int quadrilength1, int quadrilength2, int quadrilength3, int quadrilength4) {
+
+        return (quadrilength1 + quadrilength2 + quadrilength3 + quadrilength4);//perimeter of quadrilateral
+    }
+
+
 }
+
