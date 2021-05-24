c171b50aa17c70c08fec575cbfdccaa52217d420
Roxana Mehryar-Rad <roxanamehryar-rad@Roxanas-MacBook-Pro.local>
2021-05-24 13:11:13 +0300

    add homework 2105 homework examples

--- src/md/tekwill/homework2105/DaysAlive.java
+++ src/md/tekwill/homework2105/DaysAlive.java
@@ -1,5 +1,7 @@
 package md.tekwill.homework2105;
 
+import java.time.LocalTime;
+import java.util.Date;
 import java.time.Month;
 import java.time.Period;
 import java.time.LocalDate;
@@ -7,12 +9,18 @@ import java.time.temporal.ChronoUnit;
 
 public class DaysAlive {
     public static void main(String[] args) {
+        int countDaysAlive = 0;
         LocalDate birthday = LocalDate.of(2003, Month.DECEMBER, 11);
         LocalDate today = LocalDate.now();
-        System.out.println(birthday.until(today, ChronoUnit.DAYS));
+
+        while (!birthday.equals(today)) {
+            birthday = birthday.plusDays(1);
+            countDaysAlive++;
+        }
+        //System.out.println(birthday.until(today, ChronoUnit.DAYS));// another method
         //Period days = Period.between(birthday, today);
         //System.out.println(days.getYears()*365 + days.getMonths()*30 + days.getDays());
-        // System.out.println("Days alive: " + days);
+        System.out.println("Days alive: " + countDaysAlive);
 
     }
 }
