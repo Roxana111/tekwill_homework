27173a9a4fa3559e7f9a379ddd822f69185f3762
Roxana Mehryar-Rad <roxanamehryar-rad@Roxanas-MBP.mshome.net>
2021-05-26 11:35:55 +0300

    add homework 2605 homework examples

--- src/md/tekwill/homework2605/TeamLeader.java
+++ src/md/tekwill/homework2605/TeamLeader.java
@@ -4,20 +4,20 @@ public class TeamLeader extends ProductionWorker {
 
     private double monthlyBonus;
     private double requiredTrainingHours;
-    private double completedTrainingHours;
+    private double attendedTrainingHours;
 
     public TeamLeader(String name, String number, String hireDate, int shift, double payRate, double monthlyBonus, double required, double completed) {
         super(name, number, hireDate, shift, payRate);
         this.monthlyBonus = monthlyBonus;
         this.requiredTrainingHours = required;
-        this.completedTrainingHours = completed;
+        this.attendedTrainingHours = completed;
     }
 
     public TeamLeader(TeamLeader teamlead) {
         super(teamlead.getName(), teamlead.getNumber(), teamlead.getHireDate(), teamlead.getShift(), teamlead.getPayRate());
         this.monthlyBonus = teamlead.getMonthlyBonus();
         this.requiredTrainingHours = getRequiredTrainingHours();
-        this.completedTrainingHours = getCompletedTrainingHours();
+        this.attendedTrainingHours = getAttendedTrainingHours();
     }
 
     public double getMonthlyBonus() {
@@ -36,12 +36,12 @@ public class TeamLeader extends ProductionWorker {
         this.requiredTrainingHours = hours;
     }
 
-    public double getCompletedTrainingHours() {
-        return completedTrainingHours;
+    public double getAttendedTrainingHours() {
+        return attendedTrainingHours;
     }
 
-    public void setCompletedTrainingHours(double hours) {
-        this.completedTrainingHours = hours;
+    public void setAttendedTrainingHours(double hours) {
+        this.attendedTrainingHours = hours;
     }
 
 }
--- src/md/tekwill/homework2605/Test.java
+++ src/md/tekwill/homework2605/Test.java
@@ -2,11 +2,11 @@ package md.tekwill.homework2605;
 
 public class Test {
     public static void main(String[] args) {
-        Employee e = new Employee("Roxana", "111-M", "05/26/2021");
+        Employee e = new Employee("Roxana", "111-A", "05/26/2021");
         System.out.println(e);
-        ProductionWorker work = new ProductionWorker("Harry", "222-R", "08/11/2019", 2, 120.00);
+        ProductionWorker work = new ProductionWorker("Harry", "222-B", "08/11/2019", 2, 120.00);
         System.out.println(work);
-        ShiftSupervisor supers = new ShiftSupervisor("John", "444-S", "03/11/2008", 3200, 200);
+        ShiftSupervisor supers = new ShiftSupervisor("John", "444-C", "03/11/2008", 3200, 200);
         System.out.println(supers);
         TeamLeader teams = new TeamLeader("Garry", "555-K", "09/08/2017", 1, 130.00, 120, 50, 23);
         System.out.println(teams);
